0accc8f4dd25a437e7aee2f6cdb5c2ed825dcd82
Sambo Chea <sombochea100@gmail.com>
2018-10-20 00:27:44 +0700

    add more stages about ngfire:
    :

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'kotlinbarcamp';
+  items: AngularFirestoreCollection<any[]>;
+  user: Observable<firebase.User>;
+  msgVal = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFirestore) {
+    this.items = af.collection<any[]>('/questions', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.user = this.afAuth.authState;
+
+  }
+
+  login() {
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  logout() {
+      this.afAuth.auth.signOut();
+  }
+
+  Send(desc: string) {
+      this.items.push({ message: desc});
+      this.msgVal = '';
+  }
+
+
 }
